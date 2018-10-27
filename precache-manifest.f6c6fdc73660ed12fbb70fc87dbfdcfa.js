fe1bf096d9fad9251fcbf745fb2c8949288ff3f0
Jeremy <jeremyyoudesign@gmail.com>
2018-10-27 19:28:22 -0400

    Updates

---
+++ precache-manifest.f6c6fdc73660ed12fbb70fc87dbfdcfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "6be170bed798b60c1ed6",
+    "url": "/clickygame/static/js/main.6be170be.chunk.js"
+  },
+  {
+    "revision": "d7c3259394c4b01131ba",
+    "url": "/clickygame/static/js/1.d7c32593.chunk.js"
+  },
+  {
+    "revision": "6be170bed798b60c1ed6",
+    "url": "/clickygame/static/css/main.a4980d8a.chunk.css"
+  },
+  {
+    "revision": "7c03111e0716cf6bdd3d0b58766a2894",
+    "url": "/clickygame/index.html"
+  }
+];
